68a862d2edb7ede65cda47c1b77c6f779783656a
ChronoStellar <hendrikcarlo0@gmail.com>
2025-05-26 13:21:13 +0700

    Initial Commit

---
+++ EmoTrack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EmoTrack
+//
+//  Created by Hendrik Nicolas Carlo on 26/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EmoTrack/EmoTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmoTrackApp.swift
+//  EmoTrack
+//
+//  Created by Hendrik Nicolas Carlo on 26/05/25.
+//
+
+import SwiftUI
+
+@main
+struct EmoTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
